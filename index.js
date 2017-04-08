bc8c9b7c9da2ccceca985d54cb73f189ec8447ca
LE ROUX--COULOIGNER Solen <solen.leroux.c@gmail.com>
2017-04-08 11:22:48 +0200

    ca a pas marché

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 var TelegramBot = require('node-telegram-bot-api');
 var config = require('./config.json');
 
-var poubelle = require('./aquiletour');
+var etage = [
+    ["B301", "Stepan"],
+    ["B302", "Arnaud"],
+    ["B303", "Haoliang"],
+    ["B304", "Jiayi"],
+    ["B305", "Guillaume"],
+    ["B306", "Antoine"],
+    ["B307", "Shoulong"],
+    ["B308", "Yusu"],
+    ["B309", "Raymond"],
+    ["B310", "Edmond"],
+    ["B311", "Vincent"],
+    ["B312", "David"],
+    ["B313", "Tatiana"],
+    ["B314", "Owain"],
+    ["B315", "Gaëlle"],
+    ["B316", "Younes"],
+    ["B317", "Solen"],
+    ["B318", "Samy"],
+    ["B319", "Gabriel"],
+    ["B320", "Côme"],
+    ["B321", "Maxime"],
+    ["B322", "Kylian"],
+    ["B323", "Martin"],
+    ["B324", "Guillaume"]
+];
 
+function aQuiLeTour(date) {
+    return [etage[12], etage[13]]
+}
 
 // Setup polling way
 var bot = new TelegramBot(config.token, {polling: true});
