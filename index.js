4e67b500b825ec3d42daf7d8595a9e93cf840ec8
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-06 18:31:23 +0200

    fix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var etage = [
 function aQuiLeTour(date) {
     const debut = date - 1491177600; // on passe de Unix au nb de secondes depuis le 3 avril 2017
     const semaine = parseInt(debut / 604800); // on passe aux nombres de semaines s'étant écoulées depuis
-    const tour = (semaine % 11) + 4;
+    const tour = (semaine + 4) % 12;
     return [etage[tour * 2], etage[tour * 2 + 1]]
 }
 
